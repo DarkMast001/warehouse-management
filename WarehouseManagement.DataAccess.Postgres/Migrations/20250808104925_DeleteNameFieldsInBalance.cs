630b085ae8a0f90ddcb55e8479843e1929eae29f
Dmitry <demaster.easy@gmail.com>
2025-08-08 18:20:32 +0300

    fix: try improve data storage

---
+++ WarehouseManagement.DataAccess.Postgres/Migrations/20250808104925_DeleteNameFieldsInBalance.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarehouseManagement.DataAccess.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteNameFieldsInBalance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MeasureUnitName",
+                table: "Balance");
+
+            migrationBuilder.DropColumn(
+                name: "ResourceName",
+                table: "Balance");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MeasureUnitName",
+                table: "Balance",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResourceName",
+                table: "Balance",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
